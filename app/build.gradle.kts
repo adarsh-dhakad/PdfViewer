3aff66d4b4b00fa859c443ae0d1a8467c7f45289
Adarsh dhakad <58937745+adarsh-dhakad@users.noreply.github.com>
2023-08-20 22:32:46 +0530

    android 13 support

---
+++ app/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.adarsh.pdf"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.adarsh.pdf"
+        minSdk = 22
+        targetSdk = 33
+        versionCode = 24
+        versionName = "2.4.2"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // pdf viewer
+    implementation("com.github.mhiew:android-pdf-viewer:3.2.0-beta.3")
+
+    implementation("com.google.android.gms:play-services-ads-lite:22.2.0")
+}
